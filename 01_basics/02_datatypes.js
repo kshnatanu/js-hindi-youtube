c81d2405d91f0361c19d8c40fbfc8027dbb4c8ac
kshnatanu <121939830+kshnatanu@users.noreply.github.com>
2025-02-10 06:57:52 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,25 @@
+"use strict";//treat all js code as newer version
+
+//alert(3+3)//we are using nodejs,not browser
+
+let name="hitesh"
+let age=18
+let isLoggedIn=false
+
+
+//number => 2 to power 53
+//bigint
+//string => ""
+//boolean =>true/false
+//null=>standalone value
+//undefined=>
+//symbol =>unique
+
+
+//Object
+
+console.log(typeof "hitesh");
+console.log(typeof undefined);//undefined
+console.log(typeof null);//object
+
+
