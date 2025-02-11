5c03309af4b521a4300906860c0b32e1c2685833
kshnatanu <121939830+kshnatanu@users.noreply.github.com>
2025-02-11 06:50:57 +0000

    datatypeSummary

--- 01_basics/03_conversionOperator.js
+++ 01_basics/03_conversionOperator.js
@@ -25,7 +25,52 @@
 
  let StringNumber =String(someNumber)
 
- console.log(StringNumber);
- console.log(typeof StringNumber);
+//  console.log(StringNumber);
+//  console.log(typeof StringNumber);
+
+ //***** Operations*********/
+
+ let value=3
+ let negValue=-value
+ //console.log(negValue);
+
+//  console.log(2+2);
+//  console.log(2-2);
+//  console.log(2*2);
+//  console.log(2**3);
+//  console.log(2/3);
+//  console.log(2%3);
+
+let str1 ="hello"
+let str2 =" hitesh"
+
+let str3 = str1+ str2
+console.log(str3);
+
+// console.log("1"+2);//12
+// console.log(1+"2");//12
+// console.log(("1"+ 2 + 2));//122
+// console.log(1+ 2 + "2");//32
+
+// console.log(+true);
+
+let num1, num2, num3
+
+num1=num2=num3=2 + 2
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
+
+
+
+
+
+
+ 
+ 
+ 
+ 
+
  
  
