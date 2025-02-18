879a38bd36b73d5359e412fce98f601d352ddf40
kshnatanu <121939830+kshnatanu@users.noreply.github.com>
2025-02-18 06:11:08 +0000

    loop

---
+++ 05_Iterations/two.js
@@ -0,0 +1,22 @@
+
+// let index = 0
+// while (index <= 10) {
+//     // console.log(`value of index is ${index}`);
+//     index =index+2;
+// }
+
+// let myArray = ["falsh", "batman", "supermane"]
+
+// let arr = 0;
+// while(arr< myArray.length){
+//     console.log(`value: ${myArray[arr]}`);
+//     arr=arr+1;
+// }
+
+
+// let score = 21;
+// do {
+//     console.log(`Score is : ${score}`);
+//     score++;
+    
+// }while (score<=20);
