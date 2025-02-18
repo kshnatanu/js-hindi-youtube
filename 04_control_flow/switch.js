58499f41ae3166ffbe98266c80031576d6fcbbe7
kshnatanu <121939830+kshnatanu@users.noreply.github.com>
2025-02-18 17:59:16 +0000

    Dom

--- 04_control_flow/switch.js
+++ 04_control_flow/switch.js
@@ -1,4 +1,28 @@
 
+<<<<<<< HEAD
+
+const month = 3
+
+switch (month) {
+    case 1:
+        console.log("january")
+        break;
+    case 2:
+        console.log("feb")
+        break;
+    case 3:
+        console.log("march")
+        break;
+    case 4:
+        console.log("april")
+        break;
+
+    default:
+        console.log("default case match");
+        
+        break;
+}
+=======
 
 const month = "march"
 
@@ -21,3 +45,4 @@ switch (month) {
         
         break;
 }
+>>>>>>> 879a38bd36b73d5359e412fce98f601d352ddf40
